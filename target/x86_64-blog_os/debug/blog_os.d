c49a6759efc7a91ff34fa10fe7ec90f84d58aab7
Mimirin55 <178504.hinotori@gmail.com>
2025-11-12 11:04:26 +0900

    VGAテキストモード

--- target/x86_64-blog_os/debug/blog_os.d
+++ target/x86_64-blog_os/debug/blog_os.d
@@ -1,1 +1,1 @@
-/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/blog_os: /Users/satoumidori/blog_os/src/main.rs
+/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/blog_os: /Users/satoumidori/blog_os/src/main.rs /Users/satoumidori/blog_os/src/vga_buffer.rs
--- target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278.d
+++ target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278.d
@@ -1,5 +1,6 @@
-/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278.d: src/main.rs
+/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278.d: src/main.rs src/vga_buffer.rs
 
-/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278: src/main.rs
+/Users/satoumidori/blog_os/target/x86_64-blog_os/debug/deps/blog_os-1647f85218593278: src/main.rs src/vga_buffer.rs
 
 src/main.rs:
+src/vga_buffer.rs:
